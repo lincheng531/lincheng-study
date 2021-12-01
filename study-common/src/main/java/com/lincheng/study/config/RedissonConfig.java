d199e99dff7773da3c2119aa880625fa2293d7a5
林城 <924093347@qq.com>
2021-12-01 17:21:23 +0800

    创建common。依赖主pom。创建redission相关工具，与配置。

---
+++ study-common/src/main/java/com/lincheng/study/config/RedissonConfig.java
@@ -0,0 +1,25 @@
+package com.lincheng.study.config;
+
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @description:
+ * @author: linCheng
+ * @create: 2021-12-01 16:18
+ **/
+@Configuration
+public class RedissonConfig {
+
+    @Bean
+    public RedissonClient getRedissonClient() {
+        Config config = new Config();
+        config.useSingleServer()
+                .setAddress("redis://127.0.0.1:6379");
+        return Redisson.create(config);
+    }
+
+}
