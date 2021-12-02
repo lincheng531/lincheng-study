17440b8cf651eb0309923a93585cf0f8b044f2a5
林城 <924093347@qq.com>
2021-12-02 23:38:21 +0800

    spring cloud alibaba 服务注册与发现

--- study-cloud-alibaba-consumer/src/main/java/com/lincheng/study/StudyCloudAlibabaConsumerApplication.java
+++ study-cloud-alibaba-consumer/src/main/java/com/lincheng/study/StudyCloudAlibabaConsumerApplication.java
@@ -2,8 +2,11 @@ package com.lincheng.study;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+//开启服务注册与发现功能
+@EnableDiscoveryClient
 public class StudyCloudAlibabaConsumerApplication {
 
     public static void main(String[] args) {
