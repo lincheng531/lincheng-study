b5bce3f01f063644cca0c6a919b02db8a707d985
林城(39588) <924093347@qq.com>
2022-04-22 17:49:20 +0800

    微服务统一认证授权鉴权

---
+++ study-auth/src/main/java/com/lincheng/study/StudyAuthApplication.java
@@ -0,0 +1,13 @@
+package com.lincheng.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyAuthApplication.class, args);
+    }
+
+}
---
+++ study-auth/src/test/java/com/lincheng/study/StudyAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lincheng.study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
